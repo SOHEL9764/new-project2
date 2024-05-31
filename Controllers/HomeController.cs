5f5d346b4a6165a1bfbf20666278f338e37e0b28
sohail.ah91@gmail.com <sohail.ah91@gmail.com>
2024-05-31 13:32:57 +0530

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using ManagedIdentityDemowithkeyvault.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManagedIdentityDemowithkeyvault.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        public readonly ApplicationDbContext _db;
+        public HomeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet("getfullvalue")]
+        public IActionResult getfullvalue()
+        {
+            var obj = _db.profile.ToList();
+
+            return Ok(obj);
+        }
+        [HttpPost("postvalue")]
+        public IActionResult postvalue(profile _profile)
+        {
+            _db.profile.Add(_profile);
+            _db.SaveChanges();
+            return Ok(_profile);
+        }
+        [HttpGet("service")]
+        public IActionResult service()
+        {
+
+            return Ok("service");
+        }
+
+    }
+}
---
+++ Model/profile.cs
@@ -0,0 +1,13 @@
+﻿namespace ManagedIdentityDemowithkeyvault.Model
+{
+    public class profile
+    {
+        [Key]
+        public int id { get; set; }
+        public string? name { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public string? job { get; set; }
+        [Column(TypeName = "nchar(10)")]
+        public string? mobile { get; set; }
+    }
+}
